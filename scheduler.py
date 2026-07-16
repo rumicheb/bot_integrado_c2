e39c397503bc6649707c7f9762750d9532a2768f
David Rumiche <drumicheb@esge.edu.pe>
2026-07-15 19:46:25 -0500

    Mis Cambios

---
+++ scheduler.py
@@ -0,0 +1,17 @@
+# scheduler.py
+import schedule
+import time
+import threading
+
+def iniciar_hilo_planificador():
+    """Ejecuta el ciclo de reloj del scheduler en un hilo secundario de fondo."""
+    def bucle():
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+            
+    hilo = threading.Thread(target=bucle, daemon=True)
+    hilo.start()
+    print("⏳ Hilo del Scheduler activo y patrullando el reloj del sistema...")
+
+
